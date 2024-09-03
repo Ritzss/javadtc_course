23e5c92ead56181d619fe59a4c1dd4e0d908e92f
Ritzs <ritanshubabuta399@gmail.com>
2024-09-03 11:56:47 +0530

    new

---
+++ lecter 0/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void printPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        printPattern(n);
+    }
+}
---
+++ lecter 0/main1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class main1 {
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of lines: ");
+        int n = scanner.nextInt();
+        printPattern(n);
+    }
+}

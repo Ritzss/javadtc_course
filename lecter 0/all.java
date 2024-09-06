74001181c39dcdc039d9e91d3b8d81103beb0b3a
Ritzs <ritanshubabuta399@gmail.com>
2024-09-06 13:28:17 +0530

    new

--- lecter 0/all.java
+++ lecter 0/all.java
@@ -1,19 +1,15 @@
 public class all  {
-    public static void main(String[] args) {
-        // Initialize an array
+    public static int sarray( ) {
         int[] array = {1, 2, 3, 4, 5};
-
-        // Initialize sum variable
         int sum = 0;
-
-        // Loop through the array and add each element to the sum
         for (int i = 0; i < array.length; i++) {
             for (int j = i; j < i + 1; j++) {
                 sum += array[j];
             }
         }
-
-        // Print the sum
-        System.out.println("The sum of the array is: " + sum);
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("The sum of the array is: " + sarray() );
     }
 }

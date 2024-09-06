81d4995f6493a0f9f5c37a8f9cbb9dd5ef394cde
Ritzs <ritanshubabuta399@gmail.com>
2024-09-06 13:34:31 +0530

    new

--- lecter 0/all.java
+++ lecter 0/all.java
@@ -9,7 +9,27 @@ public class all  {
         }
         return sum;
     }
+
+    public static void ReverseArray () {
+            int[] arr = {1, 2, 3, 4, 5};
+            System.out.println("Original array: ");
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            int[] reversedArr = new int[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                reversedArr[i] = arr[arr.length - i - 1];
+            }
+            System.out.println("Reversed array: ");
+            for (int i : reversedArr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    
     public static void main(String[] args) {
         System.out.println("The sum of the array is: " + sarray() );
+        ReverseArray();
     }
 }
